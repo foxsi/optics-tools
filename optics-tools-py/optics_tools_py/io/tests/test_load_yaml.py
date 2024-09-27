f23a56b652cb6c436c250d61e492cad63d1569f8
Kristopher Cooper <kris@infra04-wg012.eduroam.wireless.umn.edu>
2024-09-27 15:29:25 -0500

    Pass test

--- optics-tools-py/optics_tools_py/io/tests/test_load_yaml.py
+++ optics-tools-py/optics_tools_py/io/tests/test_load_yaml.py
@@ -5,7 +5,7 @@ from optics_tools_py.io.load_yaml import load_optics_context
 def test_load_optics_context():
     """Test the `load_optics_context()` function. """
 
-    assert load_optics_context()["test"]!="test", \
+    assert load_optics_context()["test"]=="test", \
         "Test failed: `load_optics_context()`"
     
 if __name__=="__main__":
